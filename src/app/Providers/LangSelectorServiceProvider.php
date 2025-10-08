6ca2f3928ab9f00cfa107ff802c9345489dd3edb
Al Amrikasir <amrikasir@gmail.com>
2025-10-08 06:14:41 +0000

    fix miss named directory

--- src/app/Providers/LangSelectorServiceProvider.php
+++ src/app/Providers/LangSelectorServiceProvider.php
@@ -4,30 +4,30 @@ namespace LanguageSelector\App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
-use LanguageSelector\app\Livewire\LanguageSelector;
+use LanguageSelector\App\Livewire\LanguageSelector;
 
 class LangSelectorServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../../config/lang-selector.php', 'lang-selector');
+        $this->mergeConfigFrom(__DIR__ . '/../../../config/lang-selector.php', 'lang-selector');
     }
 
     public function boot(): void
     {
         // Register views
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'lang-selector');
+        $this->loadViewsFrom(__DIR__ . '/../../../resources/views', 'lang-selector');
 
         // Register Livewire component
         Livewire::component('language-selector', LanguageSelector::class);
 
         // Publish config & views
         $this->publishes([
-            __DIR__ . '/../../config/lang-selector.php' => config_path('lang-selector.php'),
+            __DIR__ . '/../../../config/lang-selector.php' => config_path('lang-selector.php'),
         ], 'lang-selector-config');
 
         $this->publishes([
-            __DIR__ . '/../../resources/views' => resource_path('views/vendor/lang-selector'),
+            __DIR__ . '/../../../resources/views' => resource_path('views/vendor/lang-selector'),
         ], 'lang-selector-views');
     }
 }
